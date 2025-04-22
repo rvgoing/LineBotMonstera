dc414289f9cb9d8406ccfe187b921da6fab57d8f
peter <cmkdkimo@gmail.com>
2025-04-23 07:15:42 +0800

    Update LineKey

--- LineBotAI.py
+++ LineBotAI.py
@@ -15,28 +15,26 @@ app = Flask(__name__)
 CHANNEL_ACCESS_TOKEN = "3v0P0QKM6SK5EtmW4tyzBZRgbenxgXjm9zNvPDvgOYZJRN/Z7gDp76rhOIMO/v/qmlZf6XFQGBlBC0v5sYPxcfU1JPl3i7ruOGNLn94HZgJZtnZbTojfwGANyD0Dy9rCNIH2HFXSYE9C90DMLsyjVAdB04t89/1O/w1cDnyilFU="
 CHANNEL_SECRET = "79bffdb9e50e19336e79aad0679665fc"
 
-
 # Initialize LINE Bot API and Handler
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 # Webhook for receiving LINE events
-
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers.get("X-Line-Signature")
-    if not signature:
-        abort(400)
+    # Get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
+    # Get request body as text
     body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
+    # Verify the signature and handle the request
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
-    return "OK", 200
-
+    return 'OK',200
 
 # Event handler for text messages
 @handler.add(MessageEvent, message=TextMessage)
